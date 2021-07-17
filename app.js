bcf6b99ed08949b5f9bf2ad6552c47ba67b0b4b1
JayD1912 <76484401+JayD1912@users.noreply.github.com>
2021-07-17 12:57:17 +0530

    done changes

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ const dbUrl=process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp'
 
 
 
-Mongoose.connect(dbUrl, {
+Mongoose.connect('mongodb://localhost:27017/yelp-camp', {
         useNewUrlParser: true,
         useCreateIndex: true,
         useUnifiedTopology: true,
