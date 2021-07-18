7ce975dc9a0252bde7d195b29112b873404ecf48
JayD1912 <76484401+JayD1912@users.noreply.github.com>
2021-07-18 11:14:31 +0530

    mongodbAtlas changes

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ const dbUrl=process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp'
 
 
 
-Mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+Mongoose.connect(dbUrl, {
         useNewUrlParser: true,
         useCreateIndex: true,
         useUnifiedTopology: true,
